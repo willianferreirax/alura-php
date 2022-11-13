61b7024a54f40f3b0eda688c03b257c6741cb22a
Willian Ferreira dos Santos <willian1948@hotmail.com>
2022-11-13 15:53:13 -0300

    Curso 1 - cap 2

---
+++ Curso1-inicio/tipos.php
@@ -0,0 +1,13 @@
+<?php
+
+$numero = 2;
+
+$salario = 100.30;
+
+$divisao = 10 / 3;
+
+$texto = "Olá mundo";
+
+$verdadeiro = true;
+
+echo gettype($numero);
---
+++ Curso1-inicio/variavel.php
@@ -0,0 +1,29 @@
+<?php
+
+// variaveis --------------------------------
+
+// $idade = 21;
+
+// echo $idade;
+
+//operacoes matematicas ------------------------------
+
+$soma = 2 + 2;
+
+$subtracao = 2 - 2;
+
+$multiplicacao = 2 * 2;
+
+$divisao = 2 / 2;
+
+$resto = 2 % 2;
+
+$potencia = 2 ** 2;
+
+$idade = 21;
+
+$idadeDaqui10Anos = $idade + 10;
+
+$idadeA5anos = $idade - 5;
+
+echo $idadeA5anos;
