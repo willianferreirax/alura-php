b595e0743b2027a17dc25199297e614fe65288a2
Willian Ferreira dos Santos <willian1948@hotmail.com>
2022-11-14 22:55:28 -0300

    Curso 1 - cap 3]

---
+++ Curso1-inicio/strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 21;
+
+echo 'Olá mundo, Minha idade é ' . $idade;
+
+echo "\n";
+
+echo "Olá mundo,\n minha idade é $idade" . PHP_EOL;
+
+echo "PHP_EOL é uma constante que representa o caracter de nova linha";
