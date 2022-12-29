cb9bc8b772192fb266aa3ea850d2e1aef7deb035
Willian Ferreira <willian1948@hotmail.com>
2022-12-29 01:10:38 -0300

    Desafios curso 1

---
+++ Curso1-inicio/desafios/imc.php
@@ -0,0 +1,17 @@
+<?php
+
+$peso = 70;
+$altura = 1.69;
+$imc = $peso / $altura ** 2;
+
+echo "Seu IMC é de $imc. Você está com o IMC ";
+
+if ($imc < 18.5) {
+    echo "abaixo";
+} elseif ($imc < 25) {
+    echo "dentro";
+} else {
+    echo "acima";
+}
+
+echo " do recomendado";
---
+++ Curso1-inicio/desafios/tabuada.php
@@ -0,0 +1,7 @@
+<?php
+
+for($i = 1; $i <= 10; $i++) {
+    for($j = 1; $j <= 10; $j++) {
+        echo "$i x $j = " . $i * $j . PHP_EOL;
+    }
+}
