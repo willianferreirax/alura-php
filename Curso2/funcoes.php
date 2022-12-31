8508972274a383789be3c49c706a4e0073f47827
willian.santos <willian1948@hotmail.com>
2022-12-31 20:43:53 -0300

    Curso 2 - cap 6

--- Curso2/funcoes.php
+++ Curso2/funcoes.php
@@ -30,3 +30,9 @@ function titularComLetrasMaiusculas(array &$conta)
 {
     $conta['titular'] = mb_strtoupper($conta['titular']);
 }
+
+function exibeConta(array $conta)
+{
+    ['titular' => $titular, 'saldo' => $saldo] = $conta;
+    echo "<li>Titular: $titular. Saldo: $saldo</li>";
+}
