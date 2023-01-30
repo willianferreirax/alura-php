d746774eedd32c4a39a345ef4c932cbe0e480cf4
willian.santos <willian1948@hotmail.com>
2023-01-30 13:46:58 -0300

    Curso 8 - cap 4

--- Curso8-Exceptions/pilha.php
+++ Curso8-Exceptions/pilha.php
@@ -6,10 +6,10 @@ function funcao1()
 
     try {
         funcao2();
-    } catch (RuntimeException | DivisionByZeroError $erroOuExcecao) {
-        echo $erroOuExcecao->getMessage() . PHP_EOL;
-        echo $erroOuExcecao->getLine() . PHP_EOL;
-        echo $erroOuExcecao->getTraceAsString() . PHP_EOL;
+    } catch (Throwable $problema) {
+        echo $problema->getMessage() . PHP_EOL;
+        echo $problema->getLine() . PHP_EOL;
+        echo $problema->getTraceAsString() . PHP_EOL;
     }
 
     echo 'Saindo da função 1' . PHP_EOL;
@@ -19,7 +19,8 @@ function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
 
-    throw new RuntimeException('Essa é a mensagem de exceção');
+    intdiv(1, 0);
+    throw new BadFunctionCallException('Essa é a mensagem de exceção');
 
     echo 'Saindo da função 2' . PHP_EOL;
 }
