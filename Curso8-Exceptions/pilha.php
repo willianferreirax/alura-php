99928b414c2fc9cb51d6d56ab2dbc08df5528326
willian.santos <willian1948@hotmail.com>
2023-01-30 12:40:23 -0300

    Curso 8 - cap 3

--- Curso8-Exceptions/pilha.php
+++ Curso8-Exceptions/pilha.php
@@ -3,23 +3,24 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
+
     try {
-        $array = new SplFixedArray(2);
-        $array[3] = 'Valor 1';
-    } catch (RunTimeException $e) {
-        echo 'Houve uma exceçao do tipo RunTimeException na funcao1' . PHP_EOL;
+        funcao2();
+    } catch (RuntimeException | DivisionByZeroError $erroOuExcecao) {
+        echo $erroOuExcecao->getMessage() . PHP_EOL;
+        echo $erroOuExcecao->getLine() . PHP_EOL;
+        echo $erroOuExcecao->getTraceAsString() . PHP_EOL;
     }
-    // $divisao = intdiv(5, 0); // funcao que trata os numeros somente como inteiro
-    funcao2();
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
-    for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
-    }
+
+    throw new RuntimeException('Essa é a mensagem de exceção');
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
